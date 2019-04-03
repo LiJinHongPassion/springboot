c42b75f8de2b9f31a93c039741647fb0d2d6105c
李锦洪 <1755497577@qq.com>
2019-04-03 13:06:45 +0800

    init

---
+++ springboot_mybatis_shiro_demo/src/main/java/com/example/li/springboot_mybatis_shiro_demo/SpringbootMybatisShiroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.li.springboot_mybatis_shiro_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisShiroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisShiroDemoApplication.class, args);
+    }
+
+}
---
+++ springboot_mybatis_shiro_demo/src/test/java/com/example/li/springboot_mybatis_shiro_demo/SpringbootMybatisShiroDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.li.springboot_mybatis_shiro_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMybatisShiroDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

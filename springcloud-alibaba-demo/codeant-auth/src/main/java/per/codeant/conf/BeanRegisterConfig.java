feccca026555bbfd14538f67ff88bd5f707413c4
lijinhong <18315107186@189.cn>
2021-01-08 18:04:33 +0800

    '更新'

--- springcloud-alibaba-demo/codeant-auth/src/main/java/per/codeant/conf/BeanRegisterConfig.java
+++ springcloud-alibaba-demo/codeant-auth/src/main/java/per/codeant/conf/BeanRegisterConfig.java
@@ -3,7 +3,6 @@ package per.codeant.conf;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import per.codeant.filter.MyFilter;
 
 /**

bdfca266feac8d3ac6392f54ec5c23b500426630
lijinhong <lijinhong@icloud.com>
2022-05-20 07:35:53 +0800

    import git-commit-id-plugs

--- springboot-git-commit-id-demo/src/main/java/com/example/springbootgitcommitiddemo/SpringbootGitCommitIdDemoApplication.java
+++ springboot-git-commit-id-demo/src/main/java/com/example/springbootgitcommitiddemo/SpringbootGitCommitIdDemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.springbootgitcommitiddemo;
 
+import org.apache.juli.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:git.properties")
 public class SpringbootGitCommitIdDemoApplication {
 
+    LogFactory logFactory = LogFactory.getFactory();
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGitCommitIdDemoApplication.class, args);
+
     }
 
 }

a91c7899ce6159e70cb8b52be9ffbf5606532a3d
lijinhong <lijinhong@icloud.com>
2022-05-19 19:57:30 +0800

    init project

---
+++ springboot-git-commit-id-demo/src/main/java/com/example/springbootgitcommitiddemo/SpringbootGitCommitIdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgitcommitiddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitCommitIdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGitCommitIdDemoApplication.class, args);
+    }
+
+}

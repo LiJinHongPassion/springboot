db7c4cdfb6161d3709221279e970e363ef0e025e
lijinhong <lijinhong@icloud.com>
2022-05-21 11:49:28 +0800

    init project

---
+++ springboot-hibernate-validation-demo/src/main/java/com/example/springboothibernatevalidationdemo/SpringbootHibernateValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothibernatevalidationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateValidationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHibernateValidationDemoApplication.class, args);
+    }
+
+}

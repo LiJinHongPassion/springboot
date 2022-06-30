8b5f71f0b6226255c9e01178fb57948661152d8f
lijinhong <lijinhong@icloud.com>
2022-06-30 20:26:25 +0800

    init project

---
+++ springboot-es-demo/src/main/java/com/example/springbootesdemo/SpringbootEsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEsDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-es-demo/src/test/java/com/example/springbootesdemo/SpringbootEsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
